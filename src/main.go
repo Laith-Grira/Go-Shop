86374eeb57db5642bdb219276a3093bdbec4abc0
Laith-Grira <lgrira12@gmail.com>
2022-05-13 10:45:20 -0400

    clean up

--- src/main.go
+++ src/main.go
@@ -8,27 +8,32 @@ import (
 	"strings"
 )
 
+func main() {
+	// initiate a new receipt
+	myReceipt := createReceipt()
+	promptOptions(myReceipt)
+}
+
+// getInput is a helper function used to get input from the user
 func getInput(prompt string, r *bufio.Reader) (string, error) {
 	fmt.Print(prompt)
 	input, err := r.ReadString('\n')
-
 	return strings.TrimSpace(input), err
 }
 
+// creatReceipt starts a new receipt for client
 func createReceipt() receipt {
 	reader := bufio.NewReader(os.Stdin)
-
 	clientName, _ := getInput("Create a new bill for: ", reader)
-
 	rcp := newReceipt(clientName)
 	fmt.Println("Created the bill -", rcp.clientName)
 
 	return rcp
 }
 
+// promptOptions is used to give different options to the user
 func promptOptions(rcp receipt) {
 	reader := bufio.NewReader(os.Stdin)
-
 	opt, _ := getInput("Choose option (a - add item, s - save & exit, p - pay receipt, v - vew receipt): ", reader)
 
 	switch opt {
@@ -42,12 +47,11 @@ func promptOptions(rcp receipt) {
 			promptOptions(rcp)
 		}
 		rcp.addItem(name, p)
-
 		fmt.Println("item added -", name, price)
 		promptOptions(rcp)
+
 	case "p":
 		cashString, _ := getInput("Enter cash amount ($): ", reader)
-
 		cash, err := strconv.ParseFloat(cashString, 64)
 		if err != nil {
 			fmt.Println("The cash must be a number...")
@@ -69,11 +73,4 @@ func promptOptions(rcp receipt) {
 		fmt.Println("That was not a valid option...")
 		promptOptions(rcp)
 	}
-}
-
-func main() {
-
-	myReceipt := createReceipt()
-	promptOptions(myReceipt)
-
 }
